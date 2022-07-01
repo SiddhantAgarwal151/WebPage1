d76e16b0b933669b3a00602781bf91014578c8ba
SpadeZap <72225926+SpadeZap@users.noreply.github.com>
2022-07-01 15:56:13 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -28,12 +28,10 @@ oauth.register(
 )
 
 @app.route("/")
-def home():
-    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 def index():
     flash("what's your name?")
     return render_template("index.html")
-    
+
 
 @app.route("/login")
 def login():
